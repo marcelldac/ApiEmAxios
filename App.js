2cf408d156a844eb9b387fadfcd31d110d7c5708
marcelldac <dacmarcell@proton.me>
2023-05-25 23:35:38 -0300

    Begin of work with API and using @env

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { KEY } from '@env' //Importando a variável de ambiente KEY (react-native-dotenv)
+
+import api from './src/services/api'
+
 export default function App() {
+
+  const [cars, setCars] = React.useState([]);
+
+  api.get('/cars').then((response) => {
+    //console.log(response.data)
+    console.log('Updated Cars')
+    setCars(response.data)
+  }) // Possible Unhandled Promise Rejection | [AxiosError: Network Error] <- Resolver esse erro.
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{`O valor da variável KEY é = ${KEY} `}</Text>
       <StatusBar style="auto" />
     </View>
   );
